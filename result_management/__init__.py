0e5667142e6d6d16a4d07dc0d21fabde23a6c94f
Yamin Rahman <isckra@Isckras-MacBook-Air.local>
2025-06-13 17:55:22 +0600

    Remove blueprint import and registration; use classic Flask style only

--- result_management/__init__.py
+++ result_management/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 from result_management.models.models import db, Session
 from result_management.config import Config
-from result_management.app import result_bp
+
 
 def create_result_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
-    app.register_blueprint(result_bp, url_prefix='/result')
 
     @app.route('/')
     def root_index():
